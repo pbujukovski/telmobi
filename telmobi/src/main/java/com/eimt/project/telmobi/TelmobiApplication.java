9dba8b195256a7af20c6376999554c5561e30945
Petar Bujukovski <pbujukovski@gmail.com>
2020-08-18 15:36:03 +0200

    Initial Commit

---
+++ telmobi/src/main/java/com/eimt/project/telmobi/TelmobiApplication.java
@@ -0,0 +1,13 @@
+package com.eimt.project.telmobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelmobiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelmobiApplication.class, args);
+    }
+
+}
---
+++ telmobi/src/test/java/com/eimt/project/telmobi/TelmobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eimt.project.telmobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelmobiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
